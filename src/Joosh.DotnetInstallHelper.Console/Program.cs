98e0e835d058253b1f420b09b960f66363dccc39
Josh Brunton <josh.brunton@proton.me>
2024-10-30 15:20:18 +0000

    feat: Add runtimes list functionality

--- src/Joosh.DotnetInstallHelper.Console/Program.cs
+++ src/Joosh.DotnetInstallHelper.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using Joosh.DotnetInstallHelper.Console.Sdks;
+﻿using Joosh.DotnetInstallHelper.Console.Runtimes;
+using Joosh.DotnetInstallHelper.Console.Sdks;
 using Spectre.Console;
 
 namespace Joosh.DotnetInstallHelper.Console;
@@ -8,17 +9,22 @@ class Program
     public static void Main(string[] args)
     {
         const string listInstalledSdks = "List installed SDKs";
+        const string listInstalledRuntimes = "List installed runtimes";
 
         var fn = AnsiConsole.Prompt(new SelectionPrompt<string>()
             .Title("Function:")
             .AddChoices([
-                listInstalledSdks
+                listInstalledSdks,
+                listInstalledRuntimes,
             ]));
 
         switch (fn)
         {
             case listInstalledSdks:
                 ListInstalledSdks.Execute();
+                break;
+            case listInstalledRuntimes:
+                ListInstalledRuntimes.Execute();
                 break;
         }
     }
---
+++ src/Joosh.DotnetInstallHelper.Console/Runtimes/ListInstalledRuntimes.cs
@@ -0,0 +1,18 @@
+﻿using Joosh.DotnetInstallHelper.Console.Extensions.System.Collections.Generic;
+using Spectre.Console;
+
+namespace Joosh.DotnetInstallHelper.Console.Runtimes;
+internal class ListInstalledRuntimes
+{
+    public static void Execute()
+    {
+        var raw = CommandProcessor.RunCommand("dotnet", ["--list-runtimes"]);
+
+        var lines = raw.Split('\n').Select(x => new DotnetSdkOrRuntime(x));
+
+        var table = lines.Tabulate().Title("Installed .NET runtimes");
+
+        AnsiConsole.Clear();
+        AnsiConsole.Write(table);
+    }
+}
