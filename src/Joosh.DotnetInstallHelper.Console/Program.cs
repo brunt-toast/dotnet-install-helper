ce0def85a6561bb51c725dcf5ee8c83c715ceb3a
Josh Brunton <josh.brunton@binary.ax>
2024-11-02 17:46:42 +0000

    Show .NET version on startup, offer install if not found

--- src/Joosh.DotnetInstallHelper.Console/Program.cs
+++ src/Joosh.DotnetInstallHelper.Console/Program.cs
@@ -18,6 +18,8 @@ class Program
     {
         if (!args.Contains("--skipSplashScreen")) ShowSplashScreen();
 
+        CheckDotnetInstalled();
+
         const string listInstalledSdks = "List installed SDKs";
         const string listInstalledRuntimes = "List installed runtimes";
         const string manageSource = "Manage sources";
@@ -94,7 +96,7 @@ class Program
             case removeWorkloads:
                 RemoveWorkloads.Execute();
                 break;
-            case updateWorkloads: 
+            case updateWorkloads:
                 UpdateWorkloads.Execute();
                 break;
             case goBack:
@@ -156,5 +158,134 @@ class Program
         Assembly assembly = Assembly.GetExecutingAssembly();
         FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
         return Regex.Replace(fileVersionInfo.ProductVersion ?? "Unknown", @"\+.*", "+...");
+    }
+
+    private static void CheckDotnetInstalled() 
+    {
+        Process process = new Process();
+        process.StartInfo.FileName = "dotnet";
+        process.StartInfo.Arguments = "--version";
+        process.StartInfo.RedirectStandardOutput = true;
+        process.StartInfo.RedirectStandardError = true;
+        process.StartInfo.UseShellExecute = false;
+        process.StartInfo.CreateNoWindow = true;
+
+        try
+        {
+            process.Start();
+            string output = process.StandardOutput.ReadToEnd();
+            string error = process.StandardError.ReadToEnd();
+            process.WaitForExit();
+            
+            if (process.ExitCode == 0) 
+            {
+                AnsiConsole.WriteLine($"dotnet {output}");
+            }
+            else 
+            {
+                AnsiConsole.Write(new Markup("[red]dotnet is not installed or was not found in PATH.[/]\n"));
+                AnsiConsole.WriteLine("Error");
+
+                string installDotnet = "Install .NET";
+                string exitProgram = "Quit";
+                var fn = AnsiConsole.Prompt(new SelectionPrompt<string>()
+                    .Title("Function:")
+                    .AddChoices([
+                        installDotnet,
+                        exitProgram
+                    ]));
+
+                if (fn == installDotnet) InstallDotNet();
+                else Environment.Exit(0);
+            } 
+
+        }
+        catch
+        {
+            AnsiConsole.Write(new Markup("[yellow]Warning: Could not determine dotnet CLI version[/]"));
+        }
+    }
+
+    private static void InstallDotNet() 
+    {
+#if WINDOWS
+        AnsiConsole.WriteLine("Detected Windows (.ps1 install script)");
+        string path = Path.Combine(Path.GetTempPath(), $"dotnet-install-{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}.ps1");
+        string webLocation = @"https://raw.githubusercontent.com/dotnet/install-scripts/main/src/dotnet-install.ps1";
+#else 
+        AnsiConsole.WriteLine("Detected linux/macOS (.sh install script)");
+        string path = Path.Combine(Path.GetTempPath(), $"dotnet-install-{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}.sh");
+        string webLocation = @"https://github.com/dotnet/install-scripts/blob/main/src/dotnet-install.sh";
+#endif
+
+        AnsiConsole.WriteLine("Downloading install script...");
+        using (HttpClient client = new())
+        {
+            try 
+            {
+                byte[] fileData = client.GetByteArrayAsync(webLocation).Result;
+                File.WriteAllBytes(path, fileData);
+            }
+            catch 
+            {
+                AnsiConsole.Write(new Markup("[red]Failed to download install script[/]"));
+                Environment.Exit(1);
+            }
+        }
+
+        AnsiConsole.WriteLine("Installing...");
+#if WINDOWS
+        Process process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "pwsh",
+                Arguments = $"\"{path}\"",
+                RedirectStandardOutput = true,          
+                RedirectStandardError = true,
+                UseShellExecute = false,                
+                CreateNoWindow = true                   
+            }
+        };
+#else 
+        Process process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = path,
+                RedirectStandardOutput = true,          
+                RedirectStandardError = true,
+                UseShellExecute = false,                
+                CreateNoWindow = true                   
+            }
+        };
+#endif
+        process.OutputDataReceived += (sender, args) => {
+            if (args.Data is null) return;
+            AnsiConsole.WriteLine(args.Data);
+        };
+        process.ErrorDataReceived += (sender, args) => AnsiConsole.WriteLine("ERROR: " + args.Data);
+
+        try
+        {
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            process.WaitForExit();
+
+            AnsiConsole.WriteLine();
+            AnsiConsole.Write(new Markup(process.ExitCode == 0
+                ? "Install Successful"
+                : $"Run exited with non-0 exit code: {process.ExitCode}"));
+        }
+        catch (Exception ex)
+        {
+            AnsiConsole.WriteLine("An error occurred: " + ex.Message);
+        }
+        finally
+        {
+            process.Close();
+            Environment.Exit(0);
+        }
     }
 }
